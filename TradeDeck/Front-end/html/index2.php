c8f483376fb50396013bc4078bd331d3604ec4ec
Kamila Białoskórska <136479@student.uni.opole.pl>
2025-06-05 17:53:42 +0200

    poprawki html
    tak

--- TradeDeck/Front-end/html/index2.php
+++ TradeDeck/Front-end/html/index2.php
@@ -7,37 +7,34 @@
     <link rel="stylesheet" href="css/style-index2.css">
 </head>
 <body>
- <header>
-        <nav class="navbar">
-            <div class="logo">
-                <img src="css/img/TradeDeck-Logo.png" alt="Logo">
-            </div>
-            <ul class="nav-links">
-                <li><a href="menu.php">Strona główna</a></li>
-                <li><a href="index2.php">Jak zacząć inwestować</a></li>
-                <li><a href="#">Analiza kursów</a></li>
-            </ul>
-            <div class="login-button">
-                <a href="#">Zaloguj</a>
-            </div>
-        </nav>
 
-        <div class="hero">
-            <div class="hero-overlay">
-                <h1 class="hero-text">Rozpocznij z nami
-                    <br>nauke!</h1>
-                <h5 class="hero-text2" >Naucz się słownika giełdy.
-                    <br>Dzięki stworzonym przez nam fiszką
-                    <br>zapamiętasz je bez problemu</h5>
-            </div>
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <img src="css/img/TradeDeck-Logo.png" alt="Logo">
         </div>
-    </header>
+        <ul class="nav-links">
+            <li><a href="menu.php">Strona główna</a></li>
+            <li><a href="index2.php">Jak zacząć inwestować</a></li>
+            <li><a href="#">Analiza kursów</a></li>
+        </ul>
+        <div class="login-button">
+            <a href="#">Zaloguj</a>
+        </div>
+    </nav>
+
+    <div class="hero">
+        <div class="hero-overlay">
+            <h1 class="hero-text">Rozpocznij z nami<br>naukę!</h1>
+            <h5 class="hero-text2">Naucz się słownika giełdy.<br>Dzięki stworzonym przez nas fiszkom<br>zapamiętasz je bez problemu</h5>
+        </div>
+    </div>
+</header>
 
 <div class="container">
-
     <h2>Fiszki</h2>
     <div class="options">
-
+        <!-- Można dodać selektory, filtry itd. -->
     </div>
 
     <div class="cards">
@@ -52,27 +49,26 @@
 
         <div class="sub-card">
             <div class="card-intermediate">
-
+                <!-- Zawartość dla poziomu średniego -->
             </div>
-        <div>
-    </div>
+        </div>
 
-    <div class="cards">
         <div class="sub-card">
             <div class="card-advanced">
-
+                <!-- Zawartość dla poziomu zaawansowanego -->
             </div>
         </div>
 
         <div class="sub-card">
             <div class="card-expert">
-
+                <!-- Zawartość dla poziomu eksperckiego -->
             </div>
         </div>
     </div>
 </div>
 
-   <div class="footer1">
+<footer>
+    <div class="footer1">
         <h1>Przydatne informacje</h1>
         <ul>
             <li><a href="#">Kursy walut</a></li>
@@ -83,72 +79,68 @@
             <li><a href="#">Oferta</a></li>
             <li><a href="#">Powódź 2024</a></li>
         </ul>
+    </div>
+
+    <div class="O_nas">
+        <h1>Nazwa</h1>
+        <ul>
+            <li><a href="#">O nas</a></li>
+            <li><a href="#">Kariera</a></li>
+            <li><a href="#">Aktualności</a></li>
+            <li><a href="#">Biuro prasowe</a></li>
+            <li><a href="#">System partnerski</a></li>
+            <li><a href="#">Obsługa klienta</a></li>
+        </ul>
+    </div>
+
+    <div class="Pomoc">
+        <h1>Pomoc i kontakt</h1>
+        <ul>
+            <li><a href="#">Kontaktuj się online</a></li>
+            <li><a href="#">Nasza placówka</a></li>
+            <li><a href="#">Telefon: 1 9999</a></li>
+            <p>24h / 7 dni</p>
+            <li><a href="#">Pytania i odpowiedzi</a></li>
+        </ul>
+    </div>
+
+    <div class="footer2">
+        <h1>Znajdziesz nas też na</h1>
+        <img class="facb" src="icons/facebook.png" alt="Facebook">
+        <img class="facb" src="icons/instagram.png" alt="Instagram">
+        <img class="facb" src="icons/x-twitter-s.svg" alt="X">
+        <img class="facb" src="icons/youtube.png" alt="YouTube">
+
+        <div class="wersja">
+            <h2>Wersja językowa</h2>
+            <a href="#">PL</a>
+            <a href="#">EN</a>
+            <a href="#">ES</a>
         </div>
+    </div>
 
-        <div class="O_nas">
-            <h1>Nazwa</h1>
-            <ul>
-                <li><a href="#">O nas</a></li>
-                <li><a href="#">Kariera</a></li>
-                <li><a href="#">Aktualności</a></li>
-                <li><a href="#">Biuro prasowe</a></li>
-                <li><a href="#">System partnerski</a></li>
-                <li><a href="#">Obsługa klienta</a></li>   
-            </ul>
-        </div>
-
-        <div class="Pomoc">
-            <h1>Pomoc i kontakt</h1>
-            <ul>
-                <li><a href="#">Kontaktuj się online</a></li>
-                <li><a href="#">Nasza placówka</a></li>
-                <li><a href="#">Telefon: 1 9999</a></li><p>24h / 7dni</p>
-                <li><a href="#">Pytania i odpowiedzi</a></li>
-            </ul>   
-        </div>
-
-        <div class="footer2">
-            <h1>Znajdziesz nas też na</h1>
-            <img class="facb" src="icons/facebook.png" alt="">
-            <img class="facb" src="icons/instagram.png" alt="">
-            <img class="facb" src="icons/x-twitter-s.svg" alt="">
-            <img class="facb" src="icons/youtube.png" alt="">
-
-            <div class="wersja">
-                <h2>Wersja językowa</h2>
-
-                <a href="#">PL</a>
-                <a href="#">EN</a>
-                <a href="#">ES</a>
-            </div>
-        </div>
-
-        <div class="footer3">
-            <p>2024 &copy; Nazwa. Wszelkie prawa zastrzeżone.
-            Nazwa i logo są zarejestrowanymi znakami towarowymi.</p>
-
-            <p>Opłata za połączenie z infolinią wiadomości zgodna z taryfą danego operatora.
+    <div class="footer3">
+        <p>2024 &copy; Nazwa. Wszelkie prawa zastrzeżone. Nazwa i logo są zarejestrowanymi znakami towarowymi.</p>
+        <p>Opłata za połączenie z infolinią wiadomości zgodna z taryfą danego operatora. <br>
             Słowniczek pojęć i definicji dotyczących usług reprezentatywnych, wynikających z rozporządzenia
-            <br>Ministra Rozwoju i Finansów z dnia 14 lipca 2017 r. w sprawie wykazu usług reprezentatywnych
-                powiązanych z rachunkiem płatniczym,
-            dostępny jest na stronie<br> strona.pl/PAD oraz w placówkach.</p>
+            Ministra Rozwoju i Finansów z dnia 14 lipca 2017 r. w sprawie wykazu usług reprezentatywnych
+            powiązanych z rachunkiem płatniczym, dostępny jest na stronie <br>
+            <a href="#">strona.pl/PAD</a> oraz w placówkach.</p>
+    </div>
 
-        </div>
+    <div class="footer4">
+        <img src="logo_stopka.png" alt="Logo stopki">
+        <a href="#">Regulamin serwisu</a>
+        <a href="#">Polityka prywatności</a>
+        <a href="#">Polityka przetwarzania danych osobowych (RODO)</a>
+        <a href="#">Polityka cookies</a>
+    </div>
+</footer>
 
-        <div class="footer4">
-            <img src="logo_stopka.png" alt="">
-            <a href="#">Regulamin serwisu</a>
-            <a href="#">Polityka prywatności</a>
-            <a href="#">Polityka przetwarzania danych osobowych (RODO)</a>
-            <a href="#">Polityka cookies</a>
-        </div>
-
+<div class="chatbot-button">
+    <span class="tooltip">Zapytaj nasze AI o pomoc!</span>
+    <span class="text">ChatBot</span>
 </div>
-
- <div class="chatbot-button">
-     <span class="tooltip">Zapytaj nasze AI o pomoc!</span>
-     <span class="text">ChatBot</span>
- </div>
 
 </body>
 </html>
